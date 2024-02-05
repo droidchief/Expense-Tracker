83414153862f50c09244d86c08377f56ed4f7d53
Vicviral <lovedayv7@gmail.com>
2024-02-05 11:54:57 +0100

    create expense model

---
+++ lib/expenses_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ExpensesScreen extends StatefulWidget {
+  const ExpensesScreen({super.key});
+
+  @override
+  State<ExpensesScreen> createState() => _ExpensesScreenState();
+}
+
+class _ExpensesScreenState extends State<ExpensesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
